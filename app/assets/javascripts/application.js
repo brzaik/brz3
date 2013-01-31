b5fadc661c2f67d2d9ccc30dcaebf57a0f14a99b
Brian R Zaik <brian.r.zaik@gmail.com>
2013-01-30 21:22:09 -0700

    Rounded out admin JS functions, namespaced them, and put them inside an admin.js file

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,82 +11,9 @@
 // GO AFTER THE REQUIRES BELOW.
 //
 //= require jquery_ujs
+//= require application/admin
 //= require application/bootstrap-dropdown
 //= require application/bootstrap-tooltip
 //= require application/bootstrap-popover
 //= require application/bootstrapx-clickover
 //= require application/lightzap
-
-
-$(document).ready(function() {
-  
-  if ( $("#flash-banner").html() !== "" ) {
-    $("#flash-banner").delay(3000).hide("slow");
-  }
-
-  $("body").on("click", "a.btnCancel", function(e) {
-    e.preventDefault();
-    $(this).closest(".panel.admin").remove();
-  });
-
-  $('.editFormTrigger').clickover({
-    html : true,
-    placement: 'left',
-    title: function() {
-      return $(this).attr('data-po-title');
-    },
-    content: function() {
-      var object_type = $(this).attr('data-object-type');
-      var object_id = $(this).attr('data-object-id');
-      return $("#" + object_type + "_" + object_id + "_editForm").html();
-    }
-  });
-
-  $('.addFormTrigger').clickover({
-    html : true,
-    placement: 'bottom',
-    title: function() {
-      return $(this).attr('data-po-title');
-    },
-    content: function() {
-      var object_type = $(this).attr('data-object-type');
-      var object_id = $(this).attr('data-object-id');
-      return $("#" + object_type + "_" + object_id + "_addForm").html();
-    }
-  });
-
-  turnOnHoverEvents();
-
-  $('body').on('click', '.editBtn', function(e) {
-    // freeze state in place:
-    turnOffHoverEvents();
-    $(this).closest('.admin-block').addClass('admin-shade');
-    $(this).parents('.admin-block').each(function() {
-      $(this).addClass('solid-shadow');
-    });
-  });
-
-  $('body').on('click', '.cancelBtn', function(e) {
-    e.preventDefault();
-    turnOnHoverEvents();
-    $('.admin-block').removeClass('admin-shade').removeClass('solid-shadow');
-    $('.admin-controls').hide();
-    $('#admin-drawer').hide();
-  });
-
-});
-
-
-function turnOnHoverEvents() {
-  $('body').on('mouseenter', '.admin-block', function() {
-      $(this).find("> .admin-controls").fadeIn("fast");
-  });
-  $('body').on('mouseleave', '.admin-block', function() {
-      $(this).find("> .admin-controls").fadeOut("fast");
-  });
-}
-
-function turnOffHoverEvents() {
-  $('body').off('mouseenter', '.admin-block');
-  $('body').off('mouseleave', '.admin-block');
-}
---
+++ app/assets/javascripts/application/admin.js
@@ -0,0 +1,65 @@
+$(document).ready(function() {
+
+  if ( $("#flash-banner").html() !== "" ) {
+    $("#flash-banner").delay(3000).hide("slow");
+  }
+
+  adminPageBuilder.turnOnHoverEvents();
+
+  $('body').on('click', '.editBtn', function(e) {
+    // freeze state in place:
+    adminPageBuilder.turnOffHoverEvents();
+    $(this).closest('.admin-block').addClass('admin-shade');
+    $(this).parents('.admin-block').each(function() {
+      $(this).addClass('solid-shadow');
+    });
+  });
+
+  $('body').on('click', '.cancelBtn', function(e) {
+    adminPageBuilder.resetPageBuildingUi();
+  });
+
+});
+
+$(document).keyup(function(e) {
+  if (e.keyCode == 27 && $('#admin-drawer').css('display') == 'block') {
+    adminPageBuilder.resetPageBuildingUi();
+  }
+});
+
+var adminPageBuilder = {
+  turnOnHoverEvents: function() {
+    $('body').on('mouseenter', '.admin-block', function() {
+        $(this).find("> .admin-controls").fadeIn("fast");
+    });
+    $('body').on('mouseleave', '.admin-block', function() {
+        $(this).find("> .admin-controls").fadeOut("fast");
+    });
+  },
+
+  turnOffHoverEvents: function() {
+    $('body').off('mouseenter', '.admin-block');
+    $('body').off('mouseleave', '.admin-block');
+  },
+
+  resetPageBuildingUi: function() {
+    adminPageBuilder.turnOnHoverEvents();
+    $('.admin-block').removeClass('admin-shade').removeClass('solid-shadow');
+    $('.admin-controls').hide();
+    adminPageBuilder.slideOutAdminDrawer();
+  },
+
+  slideInAdminDrawer: function() {
+    $('#admin-drawer').show().animate({
+      right: '0px'
+    }, 300, function() {});
+  },
+
+  slideOutAdminDrawer: function() {
+    $('#admin-drawer').animate({
+      right: '-402px'
+    }, 200, function() {
+      $(this).hide();
+    });
+  }
+};
