16ce596aa7e50f34cc1f2778352904c17d51c514
Brian R Zaik <brian.r.zaik@gmail.com>
2013-02-10 18:47:12 -0700

    Changed upload sizes; included images on portfolio pages

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -3,5 +3,5 @@ class Upload < ActiveRecord::Base
 
   has_many :images
 
-  has_attached_file :myfile, :styles => { :full => "986x400>", :orbit => "980x500#", :frontpage => "480x260#", :smproj => "228x150#", :thumb => "80x64#" }
+  has_attached_file :myfile, :styles => { :full => "986x400#", :orbit => "980x500#", :frontpage => "480x260#", :smproj => "228x150#", :thumb => "80x64#" }
 end
